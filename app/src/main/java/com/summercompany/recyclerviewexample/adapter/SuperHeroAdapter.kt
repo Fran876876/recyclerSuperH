9fd5d5e76b33fd30efbd6524ed594cadcb60853c
Fran876876 <fmartosmuoz@yahoo.es>
2025-03-05 19:19:48 +0100

    Adapter y ViewHolder finalisados

--- app/src/main/java/com/summercompany/recyclerviewexample/adapter/SuperHeroAdapter.kt
+++ app/src/main/java/com/summercompany/recyclerviewexample/adapter/SuperHeroAdapter.kt
@@ -3,29 +3,48 @@ package com.summercompany.recyclerviewexample.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.summercompany.recyclerviewexample.R
 import com.summercompany.recyclerviewexample.SuperHero
 
 //Esta clase coge un listado y lo convierte en una vistaReciclada//Extiende de la clase RecyclerView
 
-
 //Este ViewHolder se va a llamar con cada uno de los items del listado. con el metodo Inflater.
 
                         //aqui recibe el listado ( de objeto SuperHero en este caso)
-class SuperHeroAdapter(val superHeroList:List<SuperHero>) : RecyclerView.Adapter<SuperHeroViewHolder>() {
+class SuperHeroAdapter(private val superHeroList:List<SuperHero>) : RecyclerView.Adapter<SuperHeroViewHolder>() {
    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
-    //este metodo va a devolver al VIEWHOLDER el item_superhero POR CADA OBJETO QUE HAYA DE LA CLASE, por cada objeto que haya de superHeroe
-    //con el INFLATE.
+    //este metodo va a devolver al VIEWHOLDER el item_superhero POR CADA OBJETO QUE HAYA DE LA CLASE,
+    // por cada objeto que haya de superHeroe con el INFLATE.
 
-       val layoutInflater = LayoutInflater.from()
+       val layoutInflater = LayoutInflater.from(parent.context)    //necesita un CONTEXTO
 
+       return SuperHeroViewHolder(layoutInflater.inflate(R.layout.item_superhero, parent, false))
 
+//       val layoutInflater = LayoutInflater.from(parent.context):
+//       Aquí se está creando una variable layoutInflater que se inicializa con un objeto LayoutInflater usando el contexto
+//       del parent (probablemente un ViewGroup).
+//       El LayoutInflater se utiliza para convertir un archivo de diseño XML en un objeto View
+//       que se puede agregar a la interfaz de usuario.
+//
+//
+//       Esta línea está devolviendo una instancia de un SuperHeroViewHolder.
+//       layoutInflater.inflate(R.layout.item_superhero, parent, false):
+//       Aquí, el inflate está inflando el archivo de diseño XML item_superhero.xml para convertirlo en una vista.
+//       El tercer parámetro false significa que no se añadirá la vista inflada al parent aún (esto se maneja en otro momento).
+//       Finalmente, el SuperHeroViewHolder se crea usando esa vista inflada.
+//       En resumen, el código infla un diseño XML (que define cómo se verá un ítem de un superhéroe en la interfaz)
+//       y lo pasa al constructor de SuperHeroViewHolder, que probablemente lo usará para mostrar el ítem en un RecyclerView.
+//
 
 
 
    }
+                    //este metodo va a pasar por cada uno de los items y va a llamar a Render, y asi se transforma cada item
 
+                                        //fijate que NOS DEVUELVE LA INSTANCIA DEL VIEW HOLDER y tb la posicion
    override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
-
+        val item = superHeroList[position]
+        holder.render(item)
    }
    //RETORNA, DEVUELVE EL TAMAÑO DEL LISTADO
    override fun getItemCount(): Int {      // =superHeroList.size  otra manera del poner el return sería asi
--- app/src/main/java/com/summercompany/recyclerviewexample/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/summercompany/recyclerviewexample/adapter/SuperHeroViewHolder.kt
@@ -1,4 +1,35 @@
 package com.summercompany.recyclerviewexample.adapter
 
-class SuperHeroViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.summercompany.recyclerviewexample.R
+import com.summercompany.recyclerviewexample.SuperHero
+
+//EXTIENDE de ViewHolder
+class SuperHeroViewHolder(view: View):RecyclerView.ViewHolder(view) { //recibe una view de tipo View
+
+
+    val superHero = view.findViewById<TextView>(R.id.tvSuperHeroName)
+    val realName = view.findViewById<TextView>(R.id.tvRealName)
+    val publisher = view.findViewById<TextView>(R.id.tvPublisher)
+    val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
+
+
+//    render: Es el nombre de la función. El nombre podría sugerir que la función es responsable de "renderizar" o "mostrar"
+//    datos de un superhéroe en una interfaz de usuario.
+//    superHeroModel: SuperHero: El parámetro superHeroModel es de tipo SuperHero.
+//    Esto implica que hay una clase llamada SuperHero que contiene información sobre un superhéroe (como su nombre, su editor, etc.).
+//
+
+                                // esta FUNCION se va a llamar por cada item del listado de superHero (superHeroList)
+    fun render(superHeroModel: SuperHero) {
+        superHero.text = superHeroModel.superhero
+        realName.text = superHeroModel.realName
+        publisher.text = superHeroModel.publisher
+
+
+    }
 }
